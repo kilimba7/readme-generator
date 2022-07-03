7b35d6703f81952255d951d38f12fa133f8d434a
Kilimba <dyaul003@umn.edu>
2022-07-03 14:26:00 -0500

    got inquirer to work and my questions partially done

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,67 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-const renderLicenseBadge = license => {
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// const renderLicenseBadge = licenseBadge => {
 
-}
+// }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-const renderLicenseLink = license => {
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// const renderLicenseLink = licenseLink => {
 
-}
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const renderLicenseSection = license => {
+const renderLicenseSection = (license, licenseBadge, licenseLink) => {
+  if (!licenseSection) {
+    return '';
+  }
 
-}
-
-// TODO: Create a function to generate markdown for README
-const generateMarkdown = data => {
   return `
-  # ${data.title}
-`;
-}
+    <section id="license">
+      <h2> License Section ${license} </h2>
+      <p>Is this working?</p>
+      <a>
+      ${licenseLink}
+      <span>${licenseBadge}</span>
+      </a>
+    </section>
+  `;
+};
 
-module.exports = generateMarkdown;
+// Created a function to generate markdown for README
+
+module.exports = generateMarkdown => {
+
+  const { data, description, ...license } = generateMarkdown;
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${data.title}</h1>
+        <nav class="flex-row">
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+      
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${data.title}</h3>
+    </footer>
+  </body>
+  </html>
+  `;
+};
---
+++ index.js
@@ -0,0 +1,77 @@
+// Packages needed for this application
+const fs = require ('fs');
+const inquirer = require ('inquirer');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
+
+
+// Created an array of questions for user input
+const questions = () => {
+    return inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project! (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter any installation instructions if applicable'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any usage information if applicable'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter any contributors if applicable'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Please provide examples on how to run your application'
+    }
+    ]);
+};
+
+
+// TODO: Create a function to write README file
+questions()
+.then(data => {
+    const pageHtml = generatePage(data);
+
+    fs.writeFile('./index.html', pageHtml, err => {
+        if (err) throw new Error (err);
+
+        console.log('README created! Check out the index to see it displayed!')
+    });
+});
+
+
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
