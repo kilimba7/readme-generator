3e9b02f8faabef49bf2e20dd3e5d68986dffe28e
Kilimba <dyaul003@umn.edu>
2022-07-03 20:28:09 -0500

    test 1

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,13 +6,13 @@ function renderLicenseBadge(license) {
     return ''
   
   } else if (license === 'Apache License 2.0' ) { 
-    return "[![License]('https://img.shields.io/badge/License-Apache%20License%202.0-red')](https://opensource.org/licenses/Apache-2.0)"
+    return "[![License](https://img.shields.io/badge/License-Apache%20License%202.0-red)](https://opensource.org/licenses/Apache-2.0)"
  
   } else if (license === 'The MIT License' ) { 
-    return "[![License]('https://img.shields.io/badge/License-MIT-blue)]"
+    return "[![License](https://img.shields.io/badge/License-MIT-blue)](https://opensource.org/licenses/mit)"
   
   } else if (license === 'GNU General Public License 3.0' ) { 
-    return "[![License]('https://img.shields.io/badge/License-GNU%20General%20Public%20License%203.0-brightgreen')]"
+    return "[![License](https://img.shields.io/badge/License-GNU%20General%20Public%20License%203.0-brightgreen)](https://opensource.org/licenses/GPL-3.0)"
   }
   
 };
--- projectREADME.md
+++ projectREADME.md
@@ -1,9 +1,9 @@
-# ReadMe
+# Readme
 
   ## Description:
-  sdfdsgdsr
+  sdfsfd
 
-  [![License]('https://img.shields.io/badge/License-Apache%20License%202.0-red')](https://opensource.org/licenses/Apache-2.0)
+  [![License](https://img.shields.io/badge/License-Apache%20License%202.0-red)](https://opensource.org/licenses/Apache-2.0)
 
   ## Table of Contents:
   - [Installation](#installation)
@@ -29,14 +29,14 @@
 
 
   ## Features
-  sdfsdf
+  dsfds
 
   ## Contributing
-  sdfdsf
+  
 
   ## Tests
-  sdfsdf
+  
 
   ## Questions
-  - Github Username: https://github.com/kilimba7
-  - Please feel free to email me: saf
+  - Github Username: https://github.com/Kilimba7
+  - Please feel free to email me: sf
